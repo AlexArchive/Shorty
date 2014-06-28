cb346519b22e46687db9fe07487b676fcbd8432e
ByteBlast <byteblast@gmail.com>
2014-06-29 00:20:24 +0100

    AngularJS Client.

---
+++ Shorty/app/controllers/shortenerController.js
@@ -0,0 +1,11 @@
+﻿app.controller('shortenerController', function ($scope, $http) {
+    $scope.shorten = function (longUrl) {
+        $http.post("/", { 'UrlToShorten': longUrl })
+            .success(function (response) {
+                $scope.longUrl = "http://localhost:25040/" + response["absolouteIdentifier"];
+            })
+            .error(function () {
+                alert("Whops. The server is unavailable at this time.");
+            });
+    };
+});
