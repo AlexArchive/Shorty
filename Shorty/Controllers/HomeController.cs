e51767911824049c3c01d065dbe039571194cbf8
ByteBlast <byteblast@gmail.com>
2014-06-29 17:51:59 +0100

    Minor refactoring.
    Reduced arrow code (if nesting).
    Return IHttpResponseMessage instead of HttpResponseMessage.

--- Shorty/Controllers/HomeController.cs
+++ Shorty/Controllers/HomeController.cs
@@ -1,34 +1,35 @@
 ﻿using ServiceStack.Redis;
 using Shorty.Models;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace Shorty.Controllers
 {
     public class HomeController : ApiController
     {
-        public HttpResponseMessage Post(LongUrlModel model)
+        public IHttpActionResult Post(LongUrlModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                var thisDomainName = Request.RequestUri.ToString().TrimEnd('/');
-                if (model.LongUrl.StartsWith(thisDomainName))
-                {
-                    ModelState.AddModelError("", "This URL cannot be shortened. Please try another one.");
-                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                }
-
-                using (var client = new RedisClient())
-                {
-                    var stringClient = client.As<string>();
-                    var index = stringClient.GetNextSequence();
-                    var absolouteIdentifier = IdentifierGenerator.Encode((int) index);
-                    stringClient.SetEntry(absolouteIdentifier, model.LongUrl);
-                    return Request.CreateResponse(HttpStatusCode.OK, new { absolouteIdentifier });
-                }
+                return BadRequest(ModelState);
             }
-            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+
+            // prevent a redirect loop, basically.
+            var hostAddress = Request.RequestUri.ToString().TrimEnd('/');
+            if (model.LongUrl.StartsWith(hostAddress))
+            {
+                ModelState.AddModelError("", "This URL cannot be shortened. Please try another one.");
+                return BadRequest(ModelState);
+            }
+
+            using (var client = new RedisClient())
+            {
+                var stringClient = client.As<string>();
+                var index = stringClient.GetNextSequence();
+                var absolouteIdentifier = IdentifierGenerator.Encode((int)index);
+                stringClient.SetEntry(absolouteIdentifier, model.LongUrl);
+
+                return Ok(new { absolouteIdentifier });
+            }
         }
     }
 }
